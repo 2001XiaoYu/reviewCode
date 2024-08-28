54b682695cfdc35097aaee8c03557fb22fa6c70f
patrickstar <971602307@qq.com>
2024-08-28 20:03:43 +0800

    feat：记账本-保存账单信息

---
+++ 记账/accounts/routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+// 导入 lowdb
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync(__dirname + '/../data/db.json')
+// 获取 db 对象
+const db = low(adapter)
+// 导入 shortid
+const shortid = require('shortid')
+
+// 记账本的列表
+router.get('/account', function(req, res, next) {
+  res.render('list')
+});
+
+// 添加记录
+router.get('/account/create', function(req, res, next) {
+  res.render('create')
+})
+
+// 新增记录
+router.post('/account', (req, res) => {
+  // 生成 id
+  let id = shortid.generate()
+  // 写入文件
+  db.get('accounts').unshift({id: id, ...req.body}).write()
+  res.send('添加记录')
+})
+module.exports = router;
